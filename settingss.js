3d73e3c33b47f312d1595cbd437af05fc9825101
Danger-ship-it <muhammedadedokun090@gmail.com>
2025-07-30 19:55:00 +0100

    Update settingss.js
    Hello

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~YvlklJqb#_k3RN5PB79ikr5-iXeKsQxsF8PmqV3V2MOWZSc1Vykg",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2347072050737", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
